8bdf4833a6872d67e89fb01d13e0aa867e74181f
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-05-19 20:55:08 -0500

    got the pins to show up

--- src/components/SingleBoard/SingleBoard.js
+++ src/components/SingleBoard/SingleBoard.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Pin from '../Pin/Pin';
+
+import boardsData from '../../helpers/data/boardsData';
+import pinsData from '../../helpers/data/pinsData';
+
 import './SingleBoard.scss';
 
 class SingleBoard extends React.Component {
@@ -9,14 +14,37 @@ class SingleBoard extends React.Component {
     setSingleBoard: PropTypes.func.isRequired,
   }
 
+  state = {
+    board: {},
+    pins: [],
+  }
+
+  componentDidMount() {
+    const { boardId } = this.props;
+    boardsData.getSingleBoard(boardId)
+      .then((request) => {
+        const board = request.data;
+        this.setState({ board });
+        pinsData.getPinsByBoardId(boardId)
+          .then((pins) => this.setState({ pins }));
+      })
+      .catch((err) => console.error('unable to get single board', err));
+  }
+
   render() {
-    const { boardId, setSingleBoard } = this.props;
+    const { setSingleBoard } = this.props;
+    const { board, pins } = this.state;
+
+    const makePins = pins.map((p) => <Pin key={p.id} pin={p}/>);
 
     return (
       <div className="SingleBoard">
         <button className="btn btn-danger" onClick={() => { setSingleBoard(''); }}>X</button>
-        <h2>SingleBoard view</h2>
-        <h4>{boardId}</h4>
+        <h2>{board.name} Board</h2>
+        <h3>{board.description}</h3>
+        <div className="d-flex flex-wrap">
+          {makePins}
+        </div>
       </div>
     );
   }
