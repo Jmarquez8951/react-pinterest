f4a307776705818101e858828958e2dc2baa2109
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-05-19 19:15:58 -0500

    completed read boards

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -1,11 +1,33 @@
 import React from 'react';
+
+import boardsData from '../../helpers/data/boardsData';
+import authData from '../../helpers/data/authData';
+
+import Board from '../Board/Board';
+
 import './BoardContainer.scss';
 
 class BoardContainer extends React.Component {
+  state = {
+    boards: [],
+  }
+
+  componentDidMount() {
+    boardsData.getBoardsByUid(authData.getUid())
+      .then((boards) => this.setState({ boards }))
+      .catch((err) => console.error('unable to get all boards', err));
+  }
+
   render() {
+    const { boards } = this.state;
+    const makeBoards = boards.map((board) => <Board key={board.id} board={board}/>);
+
     return (
       <div className="BoardContainer">
-        <h2>Board Container</h2>
+        <h2>Boards</h2>
+        <div className="d-flex flex-wrap">
+          {makeBoards}
+        </div>
       </div>
     );
   }
