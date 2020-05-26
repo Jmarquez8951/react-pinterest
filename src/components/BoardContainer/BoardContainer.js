f594134c4e6d556b0e69b7d98f4ce26ecbde7758
Jose Marquez <josesinmarquezpalmeros@gmail.com>
2020-05-26 18:19:59 -0500

    got the button to open the board form

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -6,6 +6,7 @@ import authData from '../../helpers/data/authData';
 
 
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 import './BoardContainer.scss';
 import smash from '../../helpers/data/smash';
@@ -17,6 +18,7 @@ class BoardContainer extends React.Component {
 
   state = {
     boards: [],
+    formOpen: false,
   }
 
   getAllBoards = () => {
@@ -36,13 +38,15 @@ class BoardContainer extends React.Component {
   }
 
   render() {
-    const { boards } = this.state;
+    const { boards, formOpen } = this.state;
     const { setSingleBoard } = this.props;
     const makeBoards = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard} removeBoard={this.removeBoard}/>);
 
     return (
       <div className="BoardContainer">
         <h2>Boards</h2>
+        <button className="btn btn-warning m-2" onClick={() => this.setState({ formOpen: true })}><i class="fas fa-plus"></i></button>
+        { formOpen ? <BoardForm/> : ''}
         <div className="d-flex flex-wrap">
           {makeBoards}
         </div>
